8ee1a56f93fd79ed46b88f0d8c04059d9083b364
mnewman20 <engr6061@gmail.com>
2023-03-10 12:45:39 -0800

    test_1

--- src/matthew_newman_roslab2/scripts/matthew_newman_safety.py
+++ src/matthew_newman_roslab2/scripts/matthew_newman_safety.py
@@ -11,7 +11,8 @@ from nav_msgs.msg import Odometry
 #from geometry_msgs.msg import TwistWithCovariance as TwistC
 from ackermann_msgs.msg import AckermannDriveStamped 
 
-speed = 0
+speed = 0.5
+TTC_threshold = 0.32
 
 def callback_scan(data):
     try:    
@@ -22,25 +23,24 @@ def callback_scan(data):
         ii = 0
         TTC_min = float('inf')
         while(angle_n <= angle_max):
-            #speed_test = 1
             range_n = data.ranges[ii]
             speed_n = speed * math.cos(angle_n)
             if (speed_n > 0):
                 TTC_n = range_n/speed_n
                 if(TTC_n < TTC_min) :
                     TTC_min = TTC_n
-            #rospy.loginfo(rospy.get_caller_id() + ' speed: %f, angle: %f, speed_n: %f, TTC_n: %f', speed_test, angle_n, speed_n, TTC_min)
             ii += 1
             angle_n += angle_inc
-            #rospy.sleep(0.02)
-        if(TTC_min < 0.32):
+        bag_pub.publish(TTC_min)
+        bag_pub_2.pubshlish(TTC_threshold)
+        if(TTC_min < TTC_threshold):
             ack_msg = AckermannDriveStamped()
             ack_msg.drive.speed = 0.0
             brake_pub.publish(ack_msg)
-            brake_bool_pub.publish(True)
+            #brake_bool_pub.publish(True)
             rospy.loginfo(rospy.get_caller_id() + ' TTC_min: %f', TTC_min)
-        else:
-            brake_bool_pub.publish(False)
+        #else:
+            #brake_bool_pub.publish(False)
         #if(TTC_min != float('inf')):
 
         #if(numpy.isnan(range_min) or numpy.isnan(range_max) or numpy.isinf(range_min) or numpy.isinf(range_max)):
@@ -52,25 +52,23 @@ def callback_scan(data):
     except:
         rospy.loginfo(rospy.get_caller_id() + ' error0')  
 
-def callback_odom(data):
-    try:       
-        global speed    
-        speed = data.twist.twist.linear.x
-    except:
-        rospy.loginfo(rospy.get_caller_id() + ' error1')  
+#def callback_odom(data):
+#    try:       
+#        global speed    
+#        speed = data.twist.twist.linear.x
+#    except:
+#        rospy.loginfo(rospy.get_caller_id() + ' error1')  
 
-#closest_pub = rospy.Publisher('closest_point', Float64)
-#farthest_pub = rospy.Publisher('farthest_point', Float64)  
-brake_pub = rospy.Publisher('brake',AckermannDriveStamped, queue_size=10)
-brake_bool_pub = rospy.Publisher('brake_bool',Bool, queue_size=10)  
-
+brake_pub = rospy.Publisher('vesc/high_level/ackermann_cmd_mux/input/nav_0',AckermannDriveStamped, queue_size=10)
+#brake_bool_pub = rospy.Publisher('brake_bool',Bool, queue_size=10)  
+bag_pub = rospy.Publisher('TTC_min',Float64, queue_size=10)
+bag_pub_2 = rospy.Pubhlisher('TTC_threshold', Float64, queue_size=10)
 
 def safety():
-    #speed = 0
     rospy.init_node('matthew_newman_safety')
     rospy.loginfo(rospy.get_caller_id() + ' E Brake Monitoring...')
     rospy.Subscriber('scan', LaserScan, callback_scan)
-    rospy.Subscriber('odom', Odometry, callback_odom)
+    #rospy.Subscriber('odom', Odometry, callback_odom)
     
     rospy.spin()
 
